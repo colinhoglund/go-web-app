8aeeecdb7caf6f55957d1f88c034eb3423c1c283
Colin Hoglund <colinhoglund@gmail.com>
2021-07-12 17:32:16 -0400

    update dockerfile

--- template/Dockerfile.sh
+++ template/Dockerfile.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cat <<EOF > "${GIT_ROOT}/Dockerfile"
-FROM golang:${GO_VERSION}
-
-ENV APP_ADDR ":${DEFAULT_APP_PORT}"
-
+FROM golang:${GO_VERSION} as build
 WORKDIR /go/src/app
 COPY . .
-
 RUN go get -d -v ./...
-RUN go install -v ./...
+RUN go build -o /go/bin/app
 
-CMD ["${CMD_NAME}"]
+FROM debian:buster-slim
+RUN apt-get update && apt-get install --yes ca-certificates
+RUN groupadd -r app && useradd --no-log-init -r -g app app
+USER app
+COPY --from=build /go/bin/app /
+ENV APP_ADDR ":${DEFAULT_APP_PORT}"
 EXPOSE ${DEFAULT_APP_PORT}
+CMD ["/app"]
 EOF
